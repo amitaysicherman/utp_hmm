9b2c9479467c3018619e28211b1d80b7a92e0a76
amitay <amitay.sich@gmail.com>
2023-06-14 11:37:47 +0300

    random mask noise

--- plm/eval.py
+++ plm/eval.py
@@ -1,10 +1,12 @@
-from train import max_len, input_size, d_model, num_layers, nhead, PhonemesDataset, mask_value, padding_value
+from train import input_size, PhonemesDataset, mask_value, padding_value
 from x_transformers import TransformerWrapper, Encoder
+import random
 import torch
-from train import  get_model
-cp_file = "./models/best.cp"
-data_path =   'TIMIT_PH.npz'#'LR960_PH.npz'
-data_len_path = "TIMIT_PH_LEN.txt" # 'LR960_PH_LEN.txt'
+from train import get_model
+
+cp_file = "./models/change_66.cp"
+data_path = 'LR960_PH.npz'
+data_len_path = 'LR960_PH_LEN.txt'
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -12,27 +14,37 @@ model = get_model()
 model.load_state_dict(torch.load(cp_file, map_location=torch.device('cpu')))
 
 model = model.to(device)
+model.eval()
+
 dataset = PhonemesDataset(data_path=data_path, data_len_path=data_len_path)
 
 for i in range(10):
     x = dataset[i]
+    y = x.clone()
+
     x = x.unsqueeze(0)
     x = x.to(device)
-    model.eval()
-    output = model(x)
-    output = output.squeeze(0)
-    predicted_labels = torch.argmax(output, dim=1)
-    mask = torch.randn(x.shape) <= 0.5
+    r = random.random()
+    mask = torch.zeros_like(x).float().uniform_(0, 1) <= r
     mask[x == padding_value] = False
-    random_tokens = torch.randint_like(x, input_size)*0+mask_value
-    y = x.clone()
+    random_tokens = torch.randint_like(x, input_size)
     x[mask] = random_tokens[mask]
     output = model(x)
-    output = output[mask]
-    y = y[mask]
-    predicted_labels = torch.argmax(output, dim=1)
-    print("masking predicted")
-    # print(y)
-    # print(predicted_labels)
-    correct_predictions = (predicted_labels == y).sum().item()
-    print("accuracy", correct_predictions / len(y))
+
+    predicted_labels = torch.argmax(output.squeeze(0), dim=1)
+    predicted_labels = predicted_labels[y != padding_value]
+
+    mask = mask[0]
+    mask = mask[y != padding_value]
+    y = y[y != padding_value]
+
+    print("----")
+    print("mask", r)
+    print("len y", len(y))
+    print("len mask", sum(mask.flatten()))
+    correct_predictions = (predicted_labels[~mask] == y[~mask]).sum().item()
+    n = len(y[~mask])
+    print("no mask", correct_predictions / n)
+    correct_predictions = (predicted_labels[mask] == y[mask]).sum().item()
+    n = len(y[mask])
+    print("acc mak", correct_predictions / len(y), len(y[mask]))
