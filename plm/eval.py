4aa0cfbb133ece2fc83361c07a2b2cf1ca8bfad6
amitay <amitay.sich@gmail.com>
2023-06-15 12:38:49 +0300

    deep eval

--- plm/eval.py
+++ plm/eval.py
@@ -1,12 +1,13 @@
-from train import input_size, PhonemesDataset, mask_value, padding_value
-from x_transformers import TransformerWrapper, Encoder
-import random
+import pandas as pd
+from train import PhonemesDataset, padding_value
 import torch
+from tqdm import tqdm
 from train import get_model
+import torch.nn.functional as F
 
-cp_file = "./models/change_66.cp"
-data_path = 'LR960_PH.npz'
-data_len_path = 'LR960_PH_LEN.txt'
+
+
+cp_file = "./models/prep_random_3.cp"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -16,37 +17,39 @@ model.load_state_dict(torch.load(cp_file, map_location=torch.device('cpu')))
 model = model.to(device)
 model.eval()
 
-dataset = PhonemesDataset(data_path=data_path, data_len_path=data_len_path)
 
-for i in range(10):
-    x = dataset[i]
-    y = x.clone()
+def eval_dataset(dataset):
+    losses = []
+    type = []
+    ps = []
+    ids = []
 
-    x = x.unsqueeze(0)
-    x = x.to(device)
-    r = random.random()
-    mask = torch.zeros_like(x).float().uniform_(0, 1) <= r
-    mask[x == padding_value] = False
-    random_tokens = torch.randint_like(x, input_size)
-    x[mask] = random_tokens[mask]
-    output = model(x)
+    for i, (x, y) in tqdm(enumerate(dataset)):
+        x = x.to(device)
+        y = y.to(device)
 
-    predicted_labels = torch.argmax(output.squeeze(0), dim=1)
-    predicted_labels = predicted_labels[y != padding_value]
+        logits = model(x.unsqueeze(0))
+        loss = F.cross_entropy(
+            logits.transpose(1, 2),
+            y.unsqueeze(0),
+            ignore_index=padding_value,
+            reduction="none"
+        )[0]
+        loss = loss[y != padding_value]
+        x = x[y != padding_value]
+        y = y[y != padding_value]
+        losses.extend(loss.detach().cpu().numpy().tolist())
+        type.extend((x != y).cpu().numpy().astype(int).tolist())
+        p = (x != y).cpu().numpy().astype(int).mean()
+        p = round(p, 1)
+        ps.extend([p] * len(loss))
+        ids.extend([i] * len(loss))
 
-    mask = mask[0]
-    mask = mask[y != padding_value]
-    y = y[y != padding_value]
-    x=x[x!=padding_value]
-    print("----")
-    print("mask", r)
-    print("len y", len(y))
-    print("len mask", sum(mask.flatten()))
-    correct_predictions = (predicted_labels[~mask] == y[~mask]).sum().item()
-    n = len(y[~mask])
-    print("no mask", correct_predictions / n)
-    correct_predictions = (predicted_labels[mask] == y[mask]).sum().item()
-    n = len(y[mask])
-    print("acc mak", correct_predictions / len(y), len(y[mask]))
+    return pd.DataFrame({"id": ids, "loss": losses, "type": type, "p": ps}).to_csv("losses.csv")
 
-    print("acc tot", sum(predicted_labels == y), sum(x == y))
+with torch.no_grad():
+    train_data = PhonemesDataset("LR960")
+    eval_dataset(train_data).to_csv("train_losses.csv")
+
+    test_data = PhonemesDataset("LRTEST")
+    eval_dataset(test_data).to_csv("test_losses.csv")
