38a858a43b717201891189295cb19a72f9a16442
amitay <amitay.sich@gmail.com>
2023-06-16 08:53:47 +0300

    deep eval

--- plm/eval.py
+++ plm/eval.py
@@ -1,13 +1,14 @@
+import random
+
 import pandas as pd
 from train import PhonemesDataset, padding_value
 import torch
 from tqdm import tqdm
 from train import get_model
 import torch.nn.functional as F
+from scipy.stats import entropy
 
-
-
-cp_file = "./models/prep_random_3.cp"
+cp_file = "./models/prep_random_small_46.cp"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -18,38 +19,56 @@ model = model.to(device)
 model.eval()
 
 
-def eval_dataset(dataset):
+def eval_dataset(dataset, tot=10_000):
     losses = []
     type = []
     ps = []
     ids = []
-
-    for i, (x, y) in tqdm(enumerate(dataset)):
+    tot = min(tot, len(dataset))
+    probs = []
+    ent_logit = []
+    ent_softmax = []
+    for _ in tqdm(range(tot)):
+        i = random.randint(0, len(dataset) - 1)
+        x, y = dataset[i]
         x = x.to(device)
         y = y.to(device)
 
         logits = model(x.unsqueeze(0))
+
         loss = F.cross_entropy(
             logits.transpose(1, 2),
             y.unsqueeze(0),
             ignore_index=padding_value,
             reduction="none"
         )[0]
+
+        probs_ = F.softmax(logits, dim=-1)[0].cpu().numpy()
+        ent_softmax_ = entropy(probs_, axis=-1)
+        ent_softmax_ = ent_softmax_[y != padding_value]
+        ent_softmax.extend(ent_softmax_.tolist())
+
+        probs_ = probs_[range(len(probs_)), y.cpu().numpy()]
+        probs_ = probs_[y != padding_value]
+        probs.extend(probs_.tolist())
+
         loss = loss[y != padding_value]
         x = x[y != padding_value]
         y = y[y != padding_value]
         losses.extend(loss.detach().cpu().numpy().tolist())
         type.extend((x != y).cpu().numpy().astype(int).tolist())
         p = (x != y).cpu().numpy().astype(int).mean()
-        p = round(p, 1)
+        p = round(p, 2)
         ps.extend([p] * len(loss))
         ids.extend([i] * len(loss))
 
-    return pd.DataFrame({"id": ids, "loss": losses, "type": type, "p": ps}).to_csv("losses.csv")
+    return pd.DataFrame({"id": ids, "loss": losses, "type": type, "p": ps, "probs": probs, "ent_softmax": ent_softmax})
+
 
 with torch.no_grad():
     train_data = PhonemesDataset("LR960")
     eval_dataset(train_data).to_csv("train_losses.csv")
 
     test_data = PhonemesDataset("LRTEST")
+
     eval_dataset(test_data).to_csv("test_losses.csv")
