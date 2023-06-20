ea7b246e255baacfba4cb7f4b4fa6e4a35e33443
amitay <amitay.sich@gmail.com>
2023-06-20 10:21:15 +0300

    get timit phonemes

--- plm/text_to_phonemes_index.py
+++ plm/text_to_phonemes_index.py
@@ -4,11 +4,8 @@ import numpy as np
 from g2p_en import G2p
 from tqdm import tqdm
 import argparse
+from mapping import phonemes_to_index
 
-phonemes_to_index = {'AA': 0, 'AE': 1, 'AH': 2, 'AO': 3, 'AW': 4, 'AY': 5, 'B': 6, 'CH': 7, 'D': 8, 'DH': 9, 'EH': 10,
-                     'ER': 11, 'EY': 12, 'F': 13, 'G': 14, 'HH': 15, 'IH': 16, 'IY': 17, 'JH': 18, 'K': 19, 'L': 20,
-                     'M': 21, 'N': 22, 'NG': 23, 'OW': 24, 'OY': 25, 'P': 26, 'R': 27, 'S': 28, 'SH': 29, 'T': 30,
-                     'TH': 31, 'UH': 32, 'UW': 33, 'V': 34, 'W': 35, 'Y': 36, 'Z': 37, 'ZH': 38}
 MAX_P = max(phonemes_to_index.values())
 
 
@@ -22,9 +19,9 @@ def raplace_random(x, p):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file', type=str, default="libri_test.txt")
-    parser.add_argument('--output_prefix', type=str, default="LRTEST")
-    parser.add_argument('--do_phonemes', type=bool, default=True)
+    parser.add_argument('--input_file', type=str, default="/home/amitay/PycharmProjects/utp_hmm/data/phonemes.txt")
+    parser.add_argument('--output_prefix', type=str, default="TMTR")
+    parser.add_argument('--do_phonemes', type=bool, default=False)
     vars = parser.parse_args()
     g2p = G2p()
     with open(vars.input_file, 'r') as file:
@@ -34,8 +31,11 @@ if __name__ == '__main__':
     all_length = []
     for line in tqdm(lines):
         line = line.strip().upper()
-        sentence = " ".join(line.split(' ')[1:])  # Extract the sentence after the tab character
-        phonemes = g2p(sentence)
+        if vars.do_phonemes:
+            sentence = " ".join(line.split(' ')[1:])  # Extract the sentence after the tab character
+            phonemes = g2p(sentence)
+        else:
+            phonemes = line.split(' ')
         phonemes = [p[:-1] if p[-1].isnumeric() else p for p in phonemes]
         phonemes = [p for p in phonemes if p != "'"]
         phonemes = [p for p in phonemes if p != " "]
---
+++ plm/timit_to_phonemes_seq.py
@@ -0,0 +1,34 @@
+import glob
+from mapping import TIMIT_61_39, phonemes_to_index
+from tqdm import tqdm
+
+
+def get_phonemes(base_dir, ignore_sil=True):
+    files = glob.glob(base_dir + "*/*/*.PHN")
+    all_phonemes = []
+    all_phonemes_indexes = []
+    for file in files:
+        with open(file, 'r') as f:
+            phonemes = [x.split()[2] for x in f.read().splitlines()]
+        phonemes = [TIMIT_61_39[phoneme] for phoneme in phonemes]
+        if ignore_sil:
+            phonemes = [p for p in phonemes if p != "sil"]
+        phonemes = [p.upper() for p in phonemes]
+        phonemes_index = [str(phonemes_to_index[p]) for p in phonemes]
+        all_phonemes.append(" ".join(phonemes))
+        all_phonemes_indexes.append(" ".join(phonemes_index))
+    return all_phonemes, all_phonemes_indexes
+
+
+def write_phonemes_to_file(base_dir, output_file):
+    all_phonemes, all_phonemes_indexes = get_phonemes(base_dir)
+    with open(output_file, 'w') as f:
+        f.write("\n".join(all_phonemes))
+    with open(output_file.replace(".txt", "_IDX.txt"), 'w') as f:
+        f.write("\n".join(all_phonemes_indexes))
+
+
+timit_train = "/cs/labs/adiyoss/amitay.sich/TIMIT/data/TRAIN/"
+write_phonemes_to_file(timit_train, "TIMIT_TRAIN_PH.txt")
+timit_test = "/cs/labs/adiyoss/amitay.sich/TIMIT/data/TEST/"
+write_phonemes_to_file(timit_test, "TIMIT_TEST_PH.txt")
