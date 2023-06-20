329e9d5af95da2fd4694a3130ec5ba74ff79c66a
amitay <amitay.sich@gmail.com>
2023-06-20 18:14:13 +0300

    tarin match

--- plm/train_match.py
+++ plm/train_match.py
@@ -89,9 +89,9 @@ for ephoc in range(ephocs):
         y = y.to(device)
         # apply the models:
         linear_output = linear_model(x)
-        # argmax_output = torch.argmax(linear_output.detach(), dim=-1)
-        argmax_output = torch.multinomial(linear_output.softmax(dim=-1).view(-1, phonemes_count), 1).view(
-            linear_output.size()[:-1])
+        argmax_output = torch.argmax(linear_output.detach(), dim=-1)
+        # argmax_output = torch.multinomial(linear_output.softmax(dim=-1).view(-1, phonemes_count), 1).view(
+        #     linear_output.size()[:-1])
 
         pretrained_output = pretrained_model(argmax_output)
         pretrained_output = pretrained_output.softmax(dim=-1)
