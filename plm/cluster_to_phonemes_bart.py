6006a4e55cc18dd035983f76ec086bd13814af47
amitay <amitay.sich@gmail.com>
2023-08-20 20:12:22 +0300

    prob noise

--- plm/cluster_to_phonemes_bart.py
+++ plm/cluster_to_phonemes_bart.py
@@ -1,4 +1,4 @@
-# sbatch --gres=gpu:4,vmem:24g --mem=75G -c8 --time=7-0 --wrap "python cluster_to_phonemes_bart.py"
+# sbatch --gres=gpu:4,vmem:24g --mem=75G -c20 --time=7-0 --wrap "python cluster_to_phonemes_bart.py"
 import random
 from torch.utils.data import Dataset, DataLoader
 from utils import args_parser, save_model_to_name, load_model
@@ -13,11 +13,16 @@ from scipy.spatial.distance import cdist
 from transformers import BartConfig, BartForConditionalGeneration
 from jiwer import wer
 
+BATCH_SIZE =  32
+LR = 1e-3
+EPOCHS = 100
+load_cp = ""
+config_name = "learn_mapping_lr_bart"
+
 ONE = 0
 SPHERE = 2
-MAX_LENGTH = 1024
+MAX_LENGTH =  1024
 
-START_END_COUNT = 2
 
 PHONEMES_LAST_TOKEN = max(phonemes_to_index.values())
 CLUSTERS_FIRST_TOKEN = PHONEMES_LAST_TOKEN + 1
@@ -25,7 +30,8 @@ N_CLUSTERS = 100
 CLUSTERS_LAST_TOKEN = CLUSTERS_FIRST_TOKEN + N_CLUSTERS
 
 PAD_TOKEN = CLUSTERS_LAST_TOKEN + 1
-START_TOKEN = PAD_TOKEN + 1
+SEP = PAD_TOKEN + 1
+START_TOKEN = SEP + 1
 END_TOKEN = START_TOKEN + 1
 N_TOKENS = END_TOKEN + 1
 
@@ -37,45 +43,41 @@ def random_gaussian(n, dim=2):
 
 
 class PhonemesDataset(Dataset):
-    def __init__(self, phonemes_file, type_, dup, target_units=N_CLUSTERS, max_len=MAX_LENGTH,
-                 sep=PAD_TOKEN, size=1_000_000):
-        self.target_units = target_units
+    def __init__(self, phonemes_file, type_, dup, max_len=MAX_LENGTH, size=1_000_000):
         with open(phonemes_file, 'r') as f:
             phonemes_data = f.readlines()
         phonemes_data = [[int(x) for x in line.strip().split()] for line in phonemes_data]
-        self.sep = sep
         self.max_len = max_len
-        self.noise_sep = target_units
         self.type = type_
         self.dup = dup
         self.size = size
 
         self.data = []
         for _ in range(size):
-            sample = []
+            sample = [START_TOKEN]
             while len(sample) < max_len:
                 sample += phonemes_data[np.random.randint(0, len(phonemes_data))]
-                sample += [sep]
-            sample = sample[:max_len]
+                sample += [SEP]
+            sample = sample[:max_len - 1] + [END_TOKEN]
             self.data.append(sample)
 
     def __len__(self):
         return len(self.data)
 
     def build_mapping_one(self):
-        assert self.target_units >= len(phonemes_to_index)
-        units_mapping = list(range(N_TOKENS))
-        units_mapping += [random.randint(0, N_TOKENS - 1) for _ in range(self.target_units - N_TOKENS)]
+        units_mapping = list(range(PHONEMES_LAST_TOKEN + 1))
+        units_mapping += [random.randint(0, PHONEMES_LAST_TOKEN) for _ in
+                          range(N_CLUSTERS - (PHONEMES_LAST_TOKEN + 1))]
         random.shuffle(units_mapping)
         units_mapping = np.array(units_mapping)
-        inv_mapping = {i: [] for i in range(N_TOKENS)}
+        inv_mapping = {i: [] for i in range(PHONEMES_LAST_TOKEN + 1)}
         for i, u in enumerate(units_mapping):
             inv_mapping[u].append(i)
         return inv_mapping
 
     def build_mapping_sphere(self):
-        phonemes = random_gaussian(N_TOKENS)
-        clusters = random_gaussian(self.target_units)
+        phonemes = random_gaussian(PHONEMES_LAST_TOKEN + 1)
+        clusters = random_gaussian(N_CLUSTERS)
         cosine_distances = 100 * (1 - cdist(phonemes, clusters, metric='cosine'))
         probabilities = softmax(cosine_distances, axis=0)
         probabilities = probabilities / np.sum(probabilities, axis=1, keepdims=True)
@@ -104,38 +106,44 @@ class PhonemesDataset(Dataset):
         else:
             length = [1] * len(clean)
 
-        final_clean = clean
+        final_clean = []
         final_noise = []
-        range_units = np.arange(self.target_units)
+        range_units = np.arange(N_CLUSTERS)
         for c in clean:
             if len(final_noise) > self.max_len - START_END_COUNT:
                 break
-            if c == self.sep:
-                final_noise.append(self.noise_sep)
+
+            if c in [START_TOKEN, END_TOKEN, PAD_TOKEN, SEP]:
+                final_clean.append(c)
+                final_noise.append(c)
             else:
+                final_clean.append(c)
                 for _ in range(length.pop()):
                     if self.type == ONE:
-                        final_noise.append(random.choice(inv_mapping[c]))
-                    elif self.type == SPHERE:
-                        final_noise.append(np.random.choice(range_units, p=inv_mapping[c]))
-                    else:
-                        raise ValueError("Unknown type")
+                        new_token = random.choice(inv_mapping[c])
+                    else:  # self.type == SPHERE:
+                        new_token = np.random.choice(range_units, p=inv_mapping[c])
 
-        if len(final_clean) < self.max_len - START_END_COUNT:
-            final_clean += [self.sep] * (self.max_len - START_END_COUNT - len(final_clean))
-            final_noise += [self.noise_sep] * (self.max_len - START_END_COUNT - len(final_noise))
+                    new_token = CLUSTERS_FIRST_TOKEN + new_token
+                    final_noise.append(new_token)
 
-        final_clean = final_clean[:self.max_len - START_END_COUNT]
-        final_noise = final_noise[:self.max_len - START_END_COUNT]
+        if len(final_clean) < MAX_LENGTH:
+            final_clean += [PAD_TOKEN] * (MAX_LENGTH-len(final_clean))
+        if len(final_noise) < MAX_LENGTH:
+            final_noise += [PAD_TOKEN] * (MAX_LENGTH-len(final_noise))
+
+        if len(final_noise) > MAX_LENGTH:
+            final_noise = final_noise[:MAX_LENGTH-1]+[END_TOKEN]
+        if len(final_clean) > MAX_LENGTH:
+            final_clean = final_clean[:MAX_LENGTH-1]+[END_TOKEN]
+
+
         return final_clean, final_noise
 
     def __getitem__(self, idx):
         clean, noise = self.add_noise(self.data[idx])
         clean = torch.LongTensor(clean)
-        clean += CLUSTERS_FIRST_TOKEN
-        noise += CLUSTERS_FIRST_TOKEN
-        clean = torch.cat([torch.LongTensor([START_TOKEN]), clean, torch.LongTensor([END_TOKEN])])
-        noise = torch.cat([torch.LongTensor([START_TOKEN]), noise, torch.LongTensor([END_TOKEN])])
+        noise = torch.LongTensor(noise)
         return clean, noise
 
 
@@ -164,7 +172,7 @@ def get_model() -> BartForConditionalGeneration:
     nhead = 12
     num_layers = 12
 
-    config = BartConfig(vocab_size=N_TOKENS, max_position_embeddings=MAX_LENGTH, encoder_layers=num_layers,
+    config = BartConfig(vocab_size=N_TOKENS + 1, max_position_embeddings=MAX_LENGTH, encoder_layers=num_layers,
                         encoder_ffn_dim=d_model,
                         encoder_attention_heads=nhead, decoder_layers=num_layers, decoder_ffn_dim=d_model,
                         decoder_attention_heads=nhead,
@@ -178,33 +186,31 @@ def get_model() -> BartForConditionalGeneration:
 
 
 if __name__ == '__main__':
-    args = args_parser()
     model = get_model()
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
     load_step = 0
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = model.to(device)
 
-    if args.load_cp:
-        model, optimizer = load_model(args.load_cp, model, optimizer)
-        load_step = int(args.load_cp.split("_")[-1].replace(".cp", ""))
+    if load_cp:
+        model, optimizer = load_model(load_cp, model, optimizer)
+        load_step = int(load_cp.split("_")[-1].replace(".cp", ""))
     print("load_step", load_step, flush=True)
     if torch.cuda.device_count() > 1:
         model = DataParallel(model)
 
     criterion = nn.CrossEntropyLoss().to(device)
-    config_name = "learn_mapping_lr_bart"
 
     curr_type = ONE
     curr_dup = False
     curr_acc = 0
     losses = []
-    for epoch in range(args.epochs):
+    for epoch in range(EPOCHS):
         curr_type, curr_dup = step_config(curr_type, curr_dup, curr_acc)
         train_dataset = PhonemesDataset(phonemes_file="data/lr_train.txt", type_=curr_type, dup=curr_dup)
         train_subset = PhonemesDatasetSubset(phonemes_file="data/lr_train.txt", type_=curr_type, dup=curr_dup)
-        train_data = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False, drop_last=True)
+        train_data = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False, drop_last=True)
         train_subset_data = DataLoader(train_subset, batch_size=1, shuffle=False, drop_last=True)
         model.train()
         for i, (x, y) in tqdm(enumerate(train_data), total=len(train_data)):
