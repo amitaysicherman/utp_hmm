16d05239850e0b0c7cd321b99487f33d10baf567
amitay <amitay.sich@gmail.com>
2023-08-29 15:32:50 +0300

    prob noise

--- plm/cluster_to_phonemes_bart.sh
+++ plm/cluster_to_phonemes_bart.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
-#SBATCH --time=0-12
+#SBATCH --time=7-00
 #SBATCH --mem=32g
 #SBATCH --cpus-per-task=4
-#SBATCH --array=1-18
+#SBATCH --array=1-8
 #SBATCH --gres=gpu,vmem:8g
-#SBATCH --killable
-#SBATCH --requeue
 #SBATCH --output=output-%A_%a.txt
 
 # Define the args list directly in this file
 readarray -t args_list <<EOL
-lr xl 1 1e-4 10 512
-lr m 1 1e-4 10 512
-lr l 1 1e-4 10 512
 lr xl 1 1e-4 50 512
-lr m 1 1e-4 50 512
-lr l 1 1e-4 50 512
-lr xl 1 1e-4 10 256
-lr m 1 1e-4 10 256
-lr l 1 1e-4 10 256
 lr xl 1 1e-4 50 256
+lr m 1 1e-4 50 512
 lr m 1 1e-4 50 256
-lr l 1 1e-4 50 256
-lr s 1 1e-4 10 1024
-lr xl 1 1e-4 10 1024
-lr l 1 1e-4 10 1024
-lr s 1 1e-4 50 1024
-lr xl 1 1e-4 50 1024
-lr l 1 1e-4 50 1024
+lr xl 1 1e-4 10 512
+lr xl 1 1e-4 10 256
+lr m 1 1e-4 10 512
+lr m 1 1e-4 10 256
 EOL
 
 ARGS="${args_list[$SLURM_ARRAY_TASK_ID-1]}"
