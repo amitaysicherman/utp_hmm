abf728ad4a38b76a09f077fc7c08955badd24f99
amitay <amitay.sich@gmail.com>
2023-10-30 01:07:59 +0200

    nnnnnnnnnnnnnnnnn

---
+++ plm/bart_phonemes_letters.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --time=7-00
+#SBATCH --mem=24g
+#SBATCH --cpus-per-task=4
+#SBATCH --array=1-3
+#SBATCH --gres=gpu,vmem:24g
+
+
+case $SLURM_ARRAY_TASK_ID in
+    1)
+      noise=0.5
+      ;;
+    2)
+      noise=0.25
+      ;;
+    3)
+      noise=0.0
+      ;;
+    *)
+        echo "Invalid array index"
+        exit 1
+        ;;
+esac
+
+python bart_phonemes_letters.py --noise "$noise"
